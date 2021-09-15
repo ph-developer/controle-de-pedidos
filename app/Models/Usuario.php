dc9581cd84b5121850c1670d08a89fdaa00e097b
Raphael <2003094@aluno.univesp.br>
2021-09-15 13:05:04 -0300

    Modelo e migração da entidade "Usuario" criada; Modelo e migração da entidade "User" (padrão do framework) removida.

---
+++ app/Models/Usuario.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Usuario extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * Atributos que podem ser atribuidos em massa (mass assignment).
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'nome',
+        'email',
+        'senha',
+    ];
+
+    /**
+     * Atributos que são ocultados quando é feita a serialização da instância do modelo.
+     *
+     * @var string[]
+     */
+    protected $hidden = [
+        'senha',
+    ];
+}
