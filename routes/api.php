7e97637f55297c4c462bde4fd3c7807bbde7318b
Raphael <2003094@aluno.univesp.br>
2021-09-14 22:11:55 -0300

    Rotas (endpoints) do Controller da entidade Pedido implementadas.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PedidoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('pedidos')->group(function() {
+    Route::get('', [PedidoController::class, 'index']);
+    Route::post('', [PedidoController::class, 'store']);
+    Route::get('{tipo}/{numero}', [PedidoController::class, 'show']);
+    Route::patch('{tipo}/{numero}', [PedidoController::class, 'update']);
+    Route::delete('{tipo}/{numero}', [PedidoController::class, 'destroy']);
 });
