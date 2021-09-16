f5a0619eeca2c8eb4a7c6869ecb8be1e6a2ec9fd
Raphael <2003094@aluno.univesp.br>
2021-09-16 20:57:42 -0300

    Atualizador do cookie CSRF implementado para rodar na inicialização do frontend e a cada 5 minutos.

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,18 @@ import router from "./router";
 import App from "./layouts/App.vue";
 
 /**
+ * Carrega o Cookie CSRF e inicia um intervalo que recarrega o Cookie CSRF
+ * a cada 5 minutos.
+ */
+
+import api from "./services/api";
+
+api.app.getCsrfCookie();
+setInterval(() => {
+    api.app.getCsrfCookie();
+}, 1000 * 60 * 5);
+
+/**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
