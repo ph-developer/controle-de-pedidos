7edd6438cec6bfd617b9d87a70c8cf5f5bc5afae
Raphael <2003094@aluno.univesp.br>
2021-09-17 20:31:45 -0300

    Estilos da página Início implementados.

--- resources/js/pages/Inicio.vue
+++ resources/js/pages/Inicio.vue
@@ -1,9 +1,14 @@
 <template>
-    <div>
-        <img src="images/brasao_prefeitura.jpg" alt="Brasão da Prefeitura Municipal de Penápolis">
-        <h1>Controle de Pedidos</h1>
-        <h2>Prefeitura Municipal de Penápolis</h2>
-        <button @click="onNavigateToLogin">Entrar na Aplicação</button>
+    <div class="container vh-100">
+        <div class="row h-100 align-items-center">
+            <div class="col text-center">
+                <img src="images/brasao_prefeitura.jpg" alt="Brasão da Prefeitura Municipal de Penápolis">
+                <h1>Controle de Pedidos</h1>
+                <h3>Prefeitura Municipal de Penápolis</h3>
+                <br>
+                <button class="btn btn-primary" @click="onNavigateToLogin">Entrar na Aplicação</button>
+            </div>
+        </div>
     </div>
 </template>
 
