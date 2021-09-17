2776e7962aae9604fb97c717010979dfb258f117
Raphael <2003094@aluno.univesp.br>
2021-09-16 22:14:32 -0300

    Página Início implementada.

--- resources/js/pages/Inicio.vue
+++ resources/js/pages/Inicio.vue
@@ -1,13 +1,20 @@
 <template>
-
+    <div>
+        <img src="images/brasao_prefeitura.jpg" alt="Brasão da Prefeitura Municipal de Penápolis">
+        <h1>Controle de Pedidos</h1>
+        <h2>Prefeitura Municipal de Penápolis</h2>
+        <button @click="onNavigateToLogin">Entrar na Aplicação</button>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "Inicio"
-}
+    name: "Inicio",
+
+    methods: {
+        onNavigateToLogin() {
+            this.$router.replace({name: "login"});
+        },
+    },
+};
 </script>
-
-<style scoped>
-
-</style>
