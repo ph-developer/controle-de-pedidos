58a80878f5eaf292922a4e1787bce2e2f343ab42
Raphael <2003094@aluno.univesp.br>
2021-09-17 06:53:31 -0300

    Endpoints para manipulação da entidade Usuario implementados; Os endpoints para manipulação das entidades Pedido e Usuarios foram inseridas no middleware 'auth:sanctum', assim, apenas usuários autenticados podem acessá-los.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,14 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/auth/usuario', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/auth/usuario', function (Request $request) {
+        return $request->user();
+    });
 
-Route::prefix('pedidos')->group(function() {
-    Route::get('', [PedidoController::class, 'index']);
-    Route::post('', [PedidoController::class, 'store']);
-    Route::get('{tipo}/{numero}', [PedidoController::class, 'show']);
-    Route::patch('{tipo}/{numero}', [PedidoController::class, 'update']);
-    Route::delete('{tipo}/{numero}', [PedidoController::class, 'destroy']);
+    Route::prefix('pedidos')->group(function() {
+        Route::get('', [PedidoController::class, 'index']);
+        Route::post('', [PedidoController::class, 'store']);
+        Route::get('{tipo}/{numero}', [PedidoController::class, 'show']);
+        Route::patch('{tipo}/{numero}', [PedidoController::class, 'update']);
+        Route::delete('{tipo}/{numero}', [PedidoController::class, 'destroy']);
+    });
+
+    Route::prefix('usuarios')->group(function() {
+        Route::get('', [UsuarioController::class, 'index']);
+        Route::post('', [UsuarioController::class, 'store']);
+        Route::get('{id}', [UsuarioController::class, 'show']);
+        Route::patch('{id}', [UsuarioController::class, 'update']);
+        Route::delete('{id}', [UsuarioController::class, 'destroy']);
+    });
 });
