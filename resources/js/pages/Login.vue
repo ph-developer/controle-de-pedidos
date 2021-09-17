36c629e58edea2d74558611386835c333a630b37
Raphael <2003094@aluno.univesp.br>
2021-09-16 21:59:56 -0300

    Página Login implementada.

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,13 +1,36 @@
 <template>
-
+    <form @submit.prevent="doLogin">
+        <input id="emailInput" type="email" required v-model="email" placeholder="E-mail">
+        <br>
+        <input id="senhaInput" type="password" required v-model="senha" placeholder="Senha">
+        <br>
+        <button id="loginButton" type="submit">Entrar</button>
+    </form>
 </template>
 
 <script>
+import api from "../services/api";
+
 export default {
-    name: "Login"
-}
+    name: "Login",
+
+    data() {
+        return {
+            email: "",
+            senha: "",
+        };
+    },
+
+    methods: {
+        async doLogin() {
+            try {
+                await api.auth.doLogin(this.email, this.senha);
+                await this.$router.replace({name: "pedidos"});
+            } catch (e) {
+                //TODO: mudar de alert para uma popup.
+                alert("E-mail ou Senha incorreto.");
+            }
+        },
+    },
+};
 </script>
-
-<style scoped>
-
-</style>
